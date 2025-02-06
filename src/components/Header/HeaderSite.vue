82503624746063144fe427597a474672711427d1
thief <thiefainedev@gmail.com>
2025-02-06 20:55:06 +0100

    feat(app): create header and delete vue default template

---
+++ src/components/Header/HeaderSite.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <span class="header-logo">💉</span>
+    <h1 class="header-title">Carnet de Vaccination</h1>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: block;
+  width: 100%;
+  height: 80px;
+  background-color: #42b983;
+  text-align: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.header-logo {
+  color: white;
+  font-size: 1.1rem;
+  margin-left: 1rem;
+}
+
+.header-title {
+  flex-grow: 1;
+  color: white;
+  margin: 0;
+  font-size: 1.1rem;
+  margin-left: -1rem;
+}
+</style>
